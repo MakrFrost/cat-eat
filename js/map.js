1b9392429afb67f6bc2160a6442f9610260301cf
MakrFrost <frostmark@ukr.net>
2022-07-03 14:51:12 +0300

    Update

--- js/map.js
+++ js/map.js
@@ -5,7 +5,8 @@ const map = L.map("map").setView([50.45313571642444, 30.450107729190155], 9);
 // ! Иконка на карте
 
 var catIcon = L.icon({
-  iconUrl: "../img/logo-desktop.png",
+  iconUrl:
+    "http://s1.iconbird.com/ico/2013/9/430/w256h2561378622468catsing2.png",
 
   iconSize: [60, 50],
   shadowAnchor: [1, 1],
